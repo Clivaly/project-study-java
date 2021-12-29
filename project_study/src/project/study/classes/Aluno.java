e5d83178aa03d159e4073de18f5aebce8c1e310b
Clivaly <clivalydev@gmail.com>
2021-12-29 17:22:38 -0300

    Gerando Listas / List method

--- project_study/src/project/study/classes/Aluno.java
+++ project_study/src/project/study/classes/Aluno.java
@@ -1,81 +1,134 @@
 package project.study.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Aluno {
-	
-		/*Atributos*/
-		private String nomeAluno;
-		private int idadeAluno;
-		private String dataNascimentoAluno;
-		private String cpfAluno;
-		private String nomeMaeAluno;
-		private String dataMatriculaAluno;
-		private String nomeEscola;
-		private String serieMatriculadoAluno;
-		
-		public Aluno(){ /*Cria dados na memória - Construtor*/									
-		}
 
-		public String getNomeAluno() {
-			return nomeAluno;
-		}
+	/* Atributos */
+	private String nomeAluno;
+	private int idadeAluno;
+	private String dataNascimentoAluno;
+	private String cpfAluno;
+	private String nomeMaeAluno;
+	private String dataMatriculaAluno;
+	private String nomeEscola;
+	private String serieMatriculadoAluno;
 
-		public void setNomeAluno(String nomeAluno) {
-			this.nomeAluno = nomeAluno;
-		}
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
 
-		public int getIdadeAluno() {
-			return idadeAluno;
-		}
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
 
-		public void setIdadeAluno(int idadeAluno) {
-			this.idadeAluno = idadeAluno;
-		}
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
 
-		public String getDataNascimentoAluno() {
-			return dataNascimentoAluno;
-		}
+	public Aluno() { /* Cria dados na memória - Construtor */
+	}
 
-		public void setDataNascimentoAluno(String dataNascimentoAluno) {
-			this.dataNascimentoAluno = dataNascimentoAluno;
-		}
+	public String getNomeAluno() {
+		return nomeAluno;
+	}
 
-		public String getCpfAluno() {
-			return cpfAluno;
-		}
+	public void setNomeAluno(String nomeAluno) {
+		this.nomeAluno = nomeAluno;
+	}
 
-		public void setCpfAluno(String cpfAluno) {
-			this.cpfAluno = cpfAluno;
-		}
+	public int getIdadeAluno() {
+		return idadeAluno;
+	}
 
-		public String getNomeMaeAluno() {
-			return nomeMaeAluno;
-		}
+	public void setIdadeAluno(int idadeAluno) {
+		this.idadeAluno = idadeAluno;
+	}
 
-		public void setNomeMaeAluno(String nomeMaeAluno) {
-			this.nomeMaeAluno = nomeMaeAluno;
-		}
+	public String getDataNascimentoAluno() {
+		return dataNascimentoAluno;
+	}
 
-		public String getDataMatriculaAluno() {
-			return dataMatriculaAluno;
-		}
+	public void setDataNascimentoAluno(String dataNascimentoAluno) {
+		this.dataNascimentoAluno = dataNascimentoAluno;
+	}
 
-		public void setDataMatriculaAluno(String dataMatriculaAluno) {
-			this.dataMatriculaAluno = dataMatriculaAluno;
-		}
+	public String getCpfAluno() {
+		return cpfAluno;
+	}
 
-		public String getNomeEscola() {
-			return nomeEscola;
-		}
+	public void setCpfAluno(String cpfAluno) {
+		this.cpfAluno = cpfAluno;
+	}
 
-		public void setNomeEscola(String nomeEscola) {
-			this.nomeEscola = nomeEscola;
-		}
+	public String getNomeMaeAluno() {
+		return nomeMaeAluno;
+	}
 
-		public String getSerieMatriculadoAluno() {
-			return serieMatriculadoAluno;
-		}
+	public void setNomeMaeAluno(String nomeMaeAluno) {
+		this.nomeMaeAluno = nomeMaeAluno;
+	}
 
-		public void setSerieMatriculadoAluno(String serieMatriculadoAluno) {
-			this.serieMatriculadoAluno = serieMatriculadoAluno;
+	public String getDataMatriculaAluno() {
+		return dataMatriculaAluno;
+	}
+
+	public void setDataMatriculaAluno(String dataMatriculaAluno) {
+		this.dataMatriculaAluno = dataMatriculaAluno;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculadoAluno() {
+		return serieMatriculadoAluno;
+	}
+
+	public void setSerieMatriculadoAluno(String serieMatriculadoAluno) {
+		this.serieMatriculadoAluno = serieMatriculadoAluno;
+	}
+
+	public double getMediaNota() {
+		return 0;
+	}
+
+	public String getAlunoStatus() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return "Aprovado";
+		} else {
+			return "Reprovado";
 		}
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nomeAluno=" + nomeAluno + ", idadeAluno=" + idadeAluno + ", dataNascimentoAluno="
+				+ dataNascimentoAluno + ", cpfAluno=" + cpfAluno + ", nomeMaeAluno=" + nomeMaeAluno
+				+ ", dataMatriculaAluno=" + dataMatriculaAluno + ", nomeEscola=" + nomeEscola
+				+ ", serieMatriculadoAluno=" + serieMatriculadoAluno + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpfAluno, nomeMaeAluno);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(cpfAluno, other.cpfAluno) && Objects.equals(nomeMaeAluno, other.nomeMaeAluno);
+	}
+
 }
--- project_study/src/project/study/executavel/Base.java
+++ project_study/src/project/study/executavel/Base.java
@@ -1,16 +1,35 @@
 package project.study.executavel;
 
+import javax.swing.JOptionPane;
+
 import project.study.classes.Aluno;
 
 public class Base {
 		public static void main(String[] args) {
 			
+				String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+				String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
+				String disciplina1 = JOptionPane.showInputDialog("disciplina1: ");
+				String nota1 = JOptionPane.showInputDialog("nota1: ");
+				String disciplina2 = JOptionPane.showInputDialog("disciplina2: ");
+				String nota2 = JOptionPane.showInputDialog("nota2: ");
+				String disciplina3 = JOptionPane.showInputDialog("disciplina3: ");
+				String nota3 = JOptionPane.showInputDialog("nota3: ");
+				String disciplina4 = JOptionPane.showInputDialog("disciplina4: ");
+				String nota4 = JOptionPane.showInputDialog("nota4: ");
+			
 				/* Aluno() = instância de um objeto no java // aluno = é a variável que faz referência ao Aluno() */
 				Aluno aluno = new Aluno();
-				aluno.setNomeAluno("Fulano");;
-				aluno.setIdadeAluno(19);;
+				aluno.setNomeAluno(nome);
+				aluno.setIdadeAluno(Integer.valueOf(idade));
 				
-				System.out.println(aluno.getNomeAluno());
-				System.out.println(aluno.getIdadeAluno());
+				
+				
+				
+				
+				System.out.println(aluno.toString());
+				System.out.println("Média do aluno" + aluno.getMediaNota());
+				System.out.println("Resultado: " + aluno.getAlunoStatus());
+				
 		}
 }
